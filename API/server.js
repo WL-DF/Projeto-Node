cb3e883e7110cca52d003ca5497c683ad7641f36
WL <wanderson.ldf11@gmail.com>
2025-04-06 23:07:38 -0300

    Add o put

--- API/server.js
+++ API/server.js
@@ -6,14 +6,42 @@ const prisma = new PrismaClient()
 const app = express();
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', (req, res) =>{
-    users.push(req.body)
+
+app.post('/usuarios', async (req, res) =>{
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios',(req, res) => {
+
+
+
+app.put('/usuarios/:id', async (req, res) =>{
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+
+
+
+
+app.get('/usuarios', async(req, res) => {
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 })
 
